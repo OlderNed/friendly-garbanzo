e8c9d43c30a2c52e185cf81a72d610e293f7701b
OlderNed <alexiver96@hotmail.com>
2020-06-18 19:14:48 +0200

    I don't know what I did

--- Board.py
+++ Board.py
@@ -1,4 +1,3 @@
-from sys import argv
 from Piece import Piece
 from Tile import Tile
 
@@ -6,8 +5,7 @@ from Tile import Tile
 class Board:
     def __init__(self):
         """Creates a new Board object."""
-        self.board = [[Tile(Piece('null'))]*8]*8
-
+        self.board = [[Tile() for i in range(8)] for i in range(8)]
 
     def print_board(self):
         """Prints the board"""
@@ -20,16 +18,23 @@ class Board:
         except ValueError:
             print('Tile occupied')
             return False
+
+        return True
         
 
 
 def test():
     board = Board()
+    print('basic board')
     board.print_board()
-
-try:
-    if argv[1] == 'test':
-        test()
-except IndexError:
-    pass
-
+    print('adding black at 1,1')
+    board.place('black', *(2,5))
+    print()
+    print('added black at 1,1')
+    board.print_board()
+    print('trying to place white at 1,1') 
+    try:
+        board.place('white', *(1,1))
+    except ValueError as e:
+        print('Exception caught:', e)
+test()
