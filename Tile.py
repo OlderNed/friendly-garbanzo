1bc2080f8a773b52486e23caaba39ed5a71f4471
OlderNed <alexiver96@hotmail.com>
2020-06-18 14:59:25 +0200

    added tile functionality

--- Tile.py
+++ Tile.py
@@ -3,6 +3,7 @@ from Piece import Piece
 
 class Tile:
     def __init__(self, piece=None):
+        """Creates a new Tile object."""
         self.piece = piece
 
     def __repr__(self):
@@ -10,6 +11,22 @@ class Tile:
             return Piece.colors.get(self.piece.color)
         return '0'
 
+    def place(self, piece):
+        """Places a piece onto the tile."""
+        if self.piece:
+            raise ValueError("Can not place on an occupied tile.")
+        else:
+            self.piece = piece
+
+    def change_color(self):
+        """Changes the color of the tile."""
+        if self.piece:
+            if self.piece.color == 'black':
+                self.piece = Piece('White')
+            else:
+                self.piece = Piece('Black')
+        else:
+            raise ValueError('Tile is empty.')
 # Testing
 def test():
     print('Test start')
@@ -27,6 +44,10 @@ def test():
     print('tile with white piece')
     print(wt)
     print('tile with black piece')
+    print(bt)
+    print('Changing color of black piece on tile bt')
+    bt.change_color()
+    print('color changed')
     print(bt)
     print('Test end')
 
