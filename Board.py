64cac596d6950c67cf4799b66929570201940add
OlderNed <alexiver96@hotmail.com>
2020-06-21 22:18:31 +0200

    name change and broken code

--- Board.py
+++ Board.py
@@ -15,31 +15,38 @@ class Board:
     def place(self, piece, x, y, alt=False):
         """Places a tile on the given coordinates"""
         self.board[x][y] = piece
-    
+    def print_board(self):
+        print(list(range(8)))
+        for i, row in enumerate(self.board): print(row, [i])
 
-    def check(self, x, y, direction='p', axis='x'):
+    def flip_rows(self, x, y, direction='p', axis='x'):
+        """Checks if any pieces can be flipped, and flips them"""
+        
         piece = self.board[x][y]
         direction_coefficient = 0
+        # apply direction for the loop
         if direction == 'p':
             direction_coefficient = 1
             end = 8
         else:
             direction_coefficient = -1
             end = 0
-
+        # chose axis 
         if axis == 'y':
             self.transpose()
+            self.print_board()
+        
 
-        # positive x direction
         pieces = list()
+        # crashes on call board.flip_rows(2, 4, 'p', 'y')
         for i in range(x + 1, end, direction_coefficient):
             c_piece = self.board[i][y]
             if str(c_piece) == '0':
                 break
             else:
                 pieces.append(((i, y)))
-        
-        if not str(pieces.pop(-1)) == '0':
+        last = pieces.pop(-1)
+        if not (str(last) == '0' or str(last) == str(piece)):
             for piece in pieces:
                 x,y = piece
                 self.board[x][y].flip()
@@ -57,14 +64,14 @@ def test():
     board = Board()
     black_pieces = [Piece('black') for _ in range(8)]
     white_pieces = [Piece('white') for _ in range(8)]
-    board.place(black_pieces.pop(), 2,2)
+    board.place(black_pieces.pop(), 2,0)
+    board.place(white_pieces.pop(), 2,1)
+    board.place(white_pieces.pop(), 2,2)
     board.place(white_pieces.pop(), 2,3)
-    board.place(white_pieces.pop(), 2,4)
-    board.place(white_pieces.pop(), 2,5)
-    board.place(black_pieces.pop(), 2,6)
-    print(board)
+    board.place(black_pieces.pop(), 2,4)
+    board.print_board()
     print('running check')
-    tiles = board.check(2,2, 'p', 'y')
+    tiles = board.flip_rows(2, 0, 'p', 'y')
     print('check done running')
     print(board)
 
